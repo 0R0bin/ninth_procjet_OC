9522a30e3c1c1a2c25229889f54e80bdd44e5a0e
Robin <robinsenechal0@gmail.com>
2023-02-13 11:59:22 +0100

    Change logic view
    TODO :
    Add Logout with this logic
    Add change password

--- LITReview/LITReview/urls.py
+++ LITReview/LITReview/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import authentication.views
+import reviews.views
+
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', LoginView.as_view(
+            template_name='authentication/login.html',
+            redirect_authenticated_user=True),
+        name='login'),
+    path('logout/', authentication.views.logout_user, name='logout'),
+    path('home/', reviews.views.home, name='home'),
 ]
