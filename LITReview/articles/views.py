014c7871b1a3d31939faf7d7896bbf4ff5315715
Robin <robinsenechal0@gmail.com>
2023-04-24 14:21:18 +0200

    [u] multiples
    Add delete ticket
    Add Create review
    Add flux part (ticket & review from user + followed)
    Add Posts part (ticket & review from user only)
    Edit Ticket
    Real front ticket

--- LITReview/articles/views.py
+++ LITReview/articles/views.py
@@ -8,28 +8,44 @@ from . import models
 
 @login_required
 def flow(request):
-    latest_ticket_list = Ticket.objects.order_by('-date_created')[:10]
-    latest_review_list = Review.objects.order_by('-date_created')[:10]
+    followed_user_ticket_list = []
+    followed_user_review_list = []
+    followed_users = request.user.followed_members.all()
+
+    for followed_user in followed_users:
+        followed_user_ticket_list = followed_user_ticket_list + list(chain(Ticket.objects.all().filter(creator=followed_user)))
+        followed_user_review_list = followed_user_review_list + list(chain(Review.objects.all().filter(user=followed_user)))
+
+    user_ticket_list = Ticket.objects.all().filter(creator=request.user)
+    user_review_list = Review.objects.all().filter(user=request.user)
+
+    final_list = sorted(chain(followed_user_ticket_list, followed_user_review_list, user_ticket_list, user_review_list), key=attrgetter('date_created'))
     context = {
-        'latest_ticket_list': latest_ticket_list, 
-        'latest_review_list': latest_review_list,
+        'final_list': final_list, 
         }
+    
     return render(request, 'articles/flow.html', context)
 
 @login_required
 def posts(request):
     ticket_list = Ticket.objects.all().filter(creator=request.user)
     review_list = Review.objects.all().filter(user=request.user)
-    final_list = sorted(chain(ticket_list, review_list),key=attrgetter('date_created'))
+    final_list = sorted(chain(ticket_list, review_list), key=attrgetter('date_created'))
     context = {
         'final_list': final_list, 
         }
     return render(request, 'articles/posts.html', context)
 
 @login_required
-def detail(request, ticket_id):
+def detail_ticket(request, ticket_id):
     ticket = get_object_or_404(Ticket, pk=ticket_id)
     return render(request, 'articles/detail_ticket.html', {'ticket': ticket})
+
+@login_required
+def del_ticket(request, ticket_id):
+    ticket = get_object_or_404(Ticket, pk=ticket_id)
+    ticket.delete()
+    return redirect('/')
 
 @login_required
 def ticket_upload(request):
